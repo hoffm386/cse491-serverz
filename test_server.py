c3a82ce49a93639741ca81e1b073ab3249c92bda
Erin Hoffman <hoffm386@msu.edu>
2014-01-30 02:59:49 -0500

    Homework 3 Changes
    The main (index) page now has a link to a form page.  This page contains two
    forms, one which sends a GET request, and one which sends a POST request.
    They both redirect to a submit page, which displays the user-entered values.
    
    server.py also has a lot of refactoring, so now there is a function for every
    different path, each with params "conn" and "args".  This is for "future
    purposes".
    
    tests_server.py has renamed tests to match the new functions, and additional
    tests to make sure the form and submit pages work correctly.  Most of the
    changes are just removing spaces, since the responses inside server.py are no
    longer nested in if statements, so they have fewer spaces out front.
    
    ChangeLog was updated for Homework 3

--- test_server.py
+++ test_server.py
@@ -27,68 +27,88 @@ class FakeConnection(object):
 
 # Test a basic GET call.
 
-def test_handle_connection_slash():
+def test_handle_index_get():
     conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
     header = 'HTTP/1.0 200 OK\r\n' + \
              'Content-type: text/html\r\n' + \
              '\r\n'
     body = """
-        <h1>Hello, world.</h1>This is hoffm386's Web server.
-        <ul>
-          <li><a href='/content'>Content</a></li>
-          <li><a href='/file'>File</a></li>
-          <li><a href='/image'>Image</a></li>
-        </ul>
-        """
+    <h1>Hello, world.</h1>This is hoffm386's Web server.
+    <ul>
+        <li><a href='/content'>Content</a></li>
+        <li><a href='/file'>File</a></li>
+        <li><a href='/image'>Image</a></li>
+        <li><a href='/form'>Form</a></li>
+    </ul>
+    """
     expected_return = header + body
-
     server.handle_connection(conn)
-
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
 
-def test_handle_connection_content():
+def test_handle_content_get():
     conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
     header = 'HTTP/1.0 200 OK\r\n' + \
              'Content-type: text/html\r\n' + \
              '\r\n'
     body = """
-        <h1>Content</h1>
-        This page will contain "content"
-        """
+    <h1>Content</h1>
+    This page will contain "content"
+    """
     expected_return = header + body
-
     server.handle_connection(conn)
-
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
 
-def test_handle_connection_file():
+def test_handle_file_get():
     conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
     header = 'HTTP/1.0 200 OK\r\n' + \
              'Content-type: text/html\r\n' + \
              '\r\n'
     body = """
-        <h1>File</h1>
-        This page will contain "file"
-        """
+    <h1>File</h1>
+    This page will contain "file"
+    """
     expected_return = header + body
-
     server.handle_connection(conn)
-
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
 
-def test_handle_connection_image():
+def test_handle_image_get():
     conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
     header = 'HTTP/1.0 200 OK\r\n' + \
              'Content-type: text/html\r\n' + \
              '\r\n'
     body = """
-        <h1>Image</h1>
-        This page will contain an "image"
-        """
+    <h1>Image</h1>
+    This page will contain an "image"
+    """
     expected_return = header + body
-
     server.handle_connection(conn)
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
 
+def test_handle_form_get():
+    conn = FakeConnection("GET /form HTTP/1.0\r\n\r\n")
+    header = 'HTTP/1.0 200 OK\r\n' + \
+             'Content-type: text/html\r\n' + \
+             '\r\n'
+    body = """
+    <div>
+        <h1>GET form</h1>
+        <form action='/submit' method='GET'>
+            <input type='text' name='firstname'>
+            <input type='text' name='lastname'>
+            <input type='submit' value='Submit'>
+        </form>
+    </div>
+    <div>
+        <h1>POST form</h1>
+        <form action='/submit' method='POST'>
+            <input type='text' name='firstname'>
+            <input type='text' name='lastname'>
+            <input type='submit' value='Submit'>
+        </form>
+    </div>
+    """
+    expected_return = header + body
+    server.handle_connection(conn)
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
 
 def test_post():
@@ -97,10 +117,47 @@ def test_post():
              'Content-type: text/html\r\n' + \
              '\r\n'
     body = """
-    <h1>Post Request</h1>
-    This is not actually what a post request will do, but I have received
+    <h1>Post Request without args</h1>
+    This is not actually what a post request should do, but I have received
     a post request
     """
     expected_return = header + body
     server.handle_connection(conn)
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_submit_get():
+    get_args = "?firstname=John&lastname=Smith"
+    conn = FakeConnection("GET /submit%s HTTP/1.1\r\n\r\n" % (get_args))
+    header = 'HTTP/1.0 200 OK\r\n' + \
+             'Content-type: text/html\r\n' + \
+             '\r\n'
+    body = """
+    <h1>Form Submission</h1>
+    Hello Mr. John Smith.
+    """
+    expected_return = header + body
+    server.handle_connection(conn)
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_submit_post():
+    conn_string = "POST /submit HTTP/1.1\r\n" + \
+                  "Host: arctic.cse.msu.edu:8987\r\n" + \
+                  "Accept: text/html,application/xhtml+xml,application/xml;" +\
+                  "q=0.9,*/*;q=0.8\r\n" + \
+                  "Referer: http://arctic.cse.msu.edu:8987/form\r\n" + \
+                  "Content-Type: application/x-www-form-urlencoded\r\n" + \
+                  "Content-Length: 29\r\n\r\n" + \
+                  "firstname=John&lastname=Smith"
+    conn = FakeConnection(conn_string)
+    header = 'HTTP/1.0 200 OK\r\n' + \
+             'Content-type: text/html\r\n' + \
+             '\r\n'
+    body = """
+    <h1>Form Submission</h1>
+    Hello Mr. John Smith.
+    """
+    expected_return = header + body
+    server.handle_connection(conn)
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+
